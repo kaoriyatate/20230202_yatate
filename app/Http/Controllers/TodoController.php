4d1f691be9a06d2aefb26816eeb6ef16c03c7448
kaoriyatate <komakoma-@mx32.tiki.ne.jp>
2023-02-21 21:52:38 +0900

    after-command

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -33,18 +33,6 @@ class TodoController extends Controller
         }
     }
 
-
-
-
-
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-
-
     /**
      * Store a newly created resource in storage.
      *
@@ -119,31 +107,8 @@ class TodoController extends Controller
         return view('find', compact('todos', 'keyword', 'tags', 'tag_id','user'));
 
         
-
-            
-    
-    }
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
 
     /**
      * Update the specified resource in storage.
